1f8d9ab1ae68de2b965a5f40a87be578d4bed7d5
Krishnendu Patra <37765333+krishdu@users.noreply.github.com>
2021-08-10 19:16:59 +0530

    Initial Commit

---
+++ js/main_script.js
@@ -0,0 +1,117 @@
+pendingTaskList = Array();
+completedTaskList = Array();
+pendingTaskCount = 0;
+completedTaskCount = 0;
+
+function addTask(){
+    let newTask = document.querySelector("#newTask").value.trim();
+    console.log(newTask);
+    if(newTask != ''){
+        pendingTaskList.push(newTask);
+        //console.log(pendingTaskList);
+        renderPendingItem();
+        document.querySelector("#newTask").value = "";
+    }else{
+        alert("Please add a task first");
+    }
+}
+
+function renderPendingItem(){
+    let pendingTaskDiv = document.querySelector("#pendingTask");
+     let i = pendingTaskList.length - 1;
+     pendingTaskCount++;
+
+    let row =  document.createElement("div");
+    row.setAttribute("class", "row p-1");
+    row.setAttribute("id",`pending-item-div-${pendingTaskCount}`);
+
+    let inputCol = document.createElement("div");
+    inputCol.setAttribute("class", "col-md-10");
+
+    let buttonCol = document.createElement("div");
+    buttonCol.setAttribute("class", "col-md-2");
+
+    let input = document.createElement("input");
+    input.setAttribute("class","form-control")
+    input.setAttribute("id",`pending-item-${pendingTaskCount}`);
+    input.setAttribute("readonly","true")
+    input.setAttribute("value", pendingTaskList[i]);
+
+    let button = document.createElement("Button");
+    button.setAttribute("class","btn btn-primary px-4");
+    button.setAttribute("id",`btn-pending-item-${pendingTaskCount}`);
+    button.innerHTML = "<i class='fa fa-check-circle' aria-hidden='true'></i>"
+    button.setAttribute("onClick",`markAsComplete('${input.value}', '${pendingTaskCount}')`);
+    
+    inputCol.appendChild(input);
+    buttonCol.appendChild(button);
+    row.appendChild(inputCol);
+    row.appendChild(buttonCol);
+    
+    pendingTaskDiv.appendChild(row);        
+     
+}
+
+  
+function markAsComplete(text, actual_index){
+    //  console.log(actual_index);
+       const index = pendingTaskList.indexOf(text);
+       if (index > -1) {
+         completedTaskList.push(text); 
+         pendingTaskList.splice(index, 1);
+         let pendingTaskDiv = document.querySelector(`#pending-item-div-${actual_index}`);
+         pendingTaskDiv.remove();
+         renderCompletedItem();
+       }
+    //    console.log(pendingTaskList);
+    //    console.log(completedTaskList);
+       
+ }
+
+function renderCompletedItem(){
+    let completedTaskDiv = document.querySelector("#completedTask");
+    let i = completedTaskList.length - 1;
+    completedTaskCount++;
+
+    let row =  document.createElement("div");
+    row.setAttribute("class", "row p-1");
+    row.setAttribute("id",`completed-item-div-${completedTaskCount}`);
+
+    let inputCol = document.createElement("div");
+    inputCol.setAttribute("class", "col-md-10");
+
+    let buttonCol = document.createElement("div");
+    buttonCol.setAttribute("class", "col-md-2");
+
+    let input = document.createElement("input");
+    input.setAttribute("class","form-control")
+    input.setAttribute("id",`completed-item-${completedTaskCount}`);
+    input.setAttribute("readonly","true")
+    input.setAttribute("value", completedTaskList[i]);
+
+    let button = document.createElement("Button");
+    button.setAttribute("class","btn btn-primary px-4");
+    button.setAttribute("id",`btn-completed-item-${completedTaskCount}`);
+    button.innerHTML = "<i class='fa fa-undo' aria-hidden='true'></i>"
+    button.setAttribute("onClick",`markAsPending('${input.value}', '${completedTaskCount}')`);
+
+    inputCol.appendChild(input);
+    buttonCol.appendChild(button);
+    row.appendChild(inputCol);
+    row.appendChild(buttonCol);
+    completedTaskDiv.appendChild(row);
+} 
+
+ function markAsPending(text, actual_index){
+       // console.log(id)
+    const index = completedTaskList.indexOf(text);
+    if (index > -1) {
+        pendingTaskList.push(text); 
+        completedTaskList.splice(index, 1);
+        let completedTaskDiv = document.querySelector(`#completed-item-div-${actual_index}`);
+        completedTaskDiv.remove();
+        renderPendingItem();
+    }
+    //    console.log(pendingTaskList);
+    //    console.log(completedTaskList);   
+ }
---
+++ readme.md
@@ -0,0 +1,55 @@
+1. UI
+
+----------- Phase-1 : Adding element in List ------------
+Step 1:
+    Define an array to hold the task
+
+Step 2: 
+    After button press
+        i.  push element in array
+        ii. console log the array
+        ii. clear the input box
+
+----------- Phase-2 : Display the task list ------------
+Step 1: Design a container
+
+Step 2: (Logic)
+       > keep a counter to uniquely assign id for every element
+       > Create a row element 
+       > Create a col for item division element
+       > Create a col for button division element 
+       > create a input tag element with readonly mode
+       > Create a button element 
+       > set attributes to these element
+       > Append button and input tag element to it's respective column
+       > Append the columns to the row
+       > Append the row element to it's parent
+       > console log button id
+
+----------- Phase-3 : Pop item from pending  task and push that element into completed task array ------------
+Step 1: 
+    Create two separate array 
+    a. pending
+    b. Completed
+    
+Step 2: 
+    > when a pending task's button is clicked 
+        >> pop the item from pending task array
+        >> push that item into completed task array
+        >> console log both the array
+
+
+----------- Phase-4 : Display the task list with two seperate container (Pending, Completed) ------------
+
+    >> repeat phase-2 for completed task container
+
+----------- Phase-5 : (UNDO) Pop item from completed task and push that element into pending task array ------------
+    > when a completed task's button is clicked 
+        >> pop the item from completed task array
+        >> push that item into pending task array
+        >> render both the container
+              
+
+
+
+ 
